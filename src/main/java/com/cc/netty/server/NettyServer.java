ad65d1fc52a4cd9630b90d67c627b00dddc9e555
chencheng <chch7458@163.com>
2019-03-22 18:32:53 +0800

    111

---
+++ src/main/java/com/cc/netty/server/NettyServer.java
@@ -0,0 +1,40 @@
+package com.cc.netty.server;
+
+import com.cc.netty.util.ConnectUtil;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Author: cc
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                        System.out.println("服务启动中");
+                    }
+                })
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ServerHandler());
+                    }
+                });
+
+        ConnectUtil.bind(serverBootstrap,8001);
+    }
+
+
+}
